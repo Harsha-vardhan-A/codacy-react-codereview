b1a432f3c957fb330a76e939add9b5b9a8903c8e
harsha <harsha@divami.com>
2023-05-10 17:28:59 +0530

    kjtyut

--- src/Child.js
+++ src/Child.js
@@ -4,12 +4,13 @@ import React, { Component } from 'react';
 class Child extends Component {
   render() {
     const { type, label, value, states } = this.props;
+    console.log("gdhdhgjhn"  )
     return (
       <div>
         <input
-                              type={type}
-                              label={label}
-                              value={value}
+          type = {type}
+          label = {label}
+          value = {value}
           onChange={(e) => states(e.target.value)}
         />
       </div>
